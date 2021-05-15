049724d4de7fa3e276bc97b4f1a267aefc13a98d
Qiannan Lyu <lvqiannan@gmail.com>
2021-05-15 16:28:10 +1200

    0.3 remove bifrost test subproject & pr for iost test passed save point
    Signed-off-by: Qiannan Lyu <lvqiannan@gmail.com>

--- example/util.ts
+++
@@ -1,8 +0,0 @@
-export function parse_producers(content: any) {
-    let producers: string[] = [];
-    for (const producer of JSON.parse(content).pendingList) {
-        // console.log(stringToHex(producer));
-        producers.push(String(producer));
-    }
-    return producers;
-}
